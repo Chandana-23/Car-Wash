5fcfa252f23ce9c15f4146b2eee1180495b421ad
CHANDANA SAMINENI <chandanasamineni23@gmail.com>
2022-11-12 18:23:52 +0530

    Delete carwash directory

--- carwash/car/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Places(models.Model):
-    place_name = models.CharField(max_length=250)
-
-class Services(models.Model):
-    service_name = models.CharField(max_length=250)
